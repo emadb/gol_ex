0148838b197c2379ddeb93cb8ebfc189d741856f
Emanuele DelBono <emanuele@codiceplastico.com>
2019-05-23 21:36:45 +0200

    better simulator

--- lib/simluator.ex
+++ lib/simluator.ex
@@ -9,14 +9,7 @@ defmodule Simulator do
     GolEx.World.create_cell({4, 3})
     GolEx.World.create_cell({5, 3})
 
-    GolEx.God.tick()
-    GolEx.God.tick()
-    GolEx.God.tick()
-    GolEx.God.tick()
-    GolEx.God.tick()
-    GolEx.God.tick()
-    GolEx.God.tick()
-    GolEx.God.tick()
+    Enum.each(1..10, fn _ -> GolEx.God.tick end)
 
     GolEx.CellRegistry.get_all_cells()
   end
@@ -28,28 +21,21 @@ defmodule Simulator do
     GolEx.World.create_cell({2, 3})
     GolEx.World.create_cell({3, 3})
 
-    GolEx.God.tick()
-    GolEx.God.tick()
-    GolEx.God.tick()
-    GolEx.God.tick()
-    GolEx.God.tick()
-    GolEx.God.tick()
-    GolEx.God.tick()
-    GolEx.God.tick()
+    Enum.each(1..10, fn _ -> GolEx.God.tick end)
 
     GolEx.CellRegistry.get_all_cells()
   end
 
-  def start_simulation_random(n) do
+  def start_simulation_random(cells, cycles) do
 
-    1..n
+    1..cells
       |> Enum.map(fn _ -> { Enum.random(1..100), Enum.random(1..100) } end)
       |> Enum.uniq
       |> Enum.map(fn cell -> GolEx.World.create_cell(cell) end)
 
     IO.inspect Enum.count(GolEx.CellRegistry.get_all_cells()), label: "Tick 0"
 
-    1..n
+    1..cycles
       |> Enum.map(fn t ->
         GolEx.God.tick()
         IO.inspect Enum.count(GolEx.CellRegistry.get_all_cells()), label: "Tick #{t}"
